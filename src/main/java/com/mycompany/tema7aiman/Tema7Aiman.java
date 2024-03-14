5583e04b9ca5cc84595e24059104670f1b41912c
Aiman <ahardao1001@g.educaand.es>
2024-03-14 12:53:29 +0100

    c1

---
+++ src/main/java/com/mycompany/tema7aiman/Tema7Aiman.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tema7aiman;
+
+/**
+ *
+ * @author aiman
+ */
+public class Tema7Aiman {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
