a56aa10ef8921fd8b24a86198b04a5d53f8a6848
Aiman <ahardao1001@g.educaand.es>
2024-03-14 12:58:22 +0100

    c2

--- src/main/java/expresiones_regulares/Ejemplo.java
+++ src/main/java/expresiones_regulares/Ejemplo.java
@@ -9,5 +9,7 @@ package expresiones_regulares;
  * @author aiman
  */
 public class Ejemplo {
-    
+    public static void main(String[] args) {
+        System.out.println("Expresiones regulares");
+    }
 }
