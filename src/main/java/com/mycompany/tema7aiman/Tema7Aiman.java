10a0d82ff6619c8add7fd0197694018412dc2e6e
Aiman <ahardao1001@g.educaand.es>
2024-03-15 10:10:31 +0100

    c2

--- src/main/java/com/mycompany/tema7aiman/Tema7Aiman.java
+++ src/main/java/com/mycompany/tema7aiman/Tema7Aiman.java
@@ -11,6 +11,6 @@ package com.mycompany.tema7aiman;
 public class Tema7Aiman {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+       //hacer proyecto leer ficheros de vuestros proyectos sacar csv de los productos 20 
     }
 }
